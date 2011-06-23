26dcb552c6d9a2809cfede3ee75bf79fd8cd3b5e
Nathaniel Mote <nmote@cs.washington.edu>
2011-06-23 13:30:45 -0700

    added detail and several features to specs

--- java/Translation/src/level/Chute.java
+++ java/Translation/src/level/Chute.java
@@ -5,17 +5,33 @@ package level;
  * 
  * A mutable structure representing chute segments.
  * 
- * Implements eternal equality because it is mutable, but must be used in Collections
+ * Implements eternal equality because it is mutable, but must be used in
+ * Collections
+ * 
+ * @specfield name : Name // The name of the variable corresponding to this
+ * chute. Can be null if this chute does not correspond directly to a variable,
+ * but all chutes connected to an incoming or outgoing node, except for the
+ * arguments and the return value, must have a name. This is because they
+ * represent fields, or, if their board is a sub-board, variables declared
+ * within the method
+ * 
+ * @specfield auxiliaryChutes: List<Chute> // The list of chutes that represent
+ * types auxiliary to the type that this chute represents
  * 
  * @specfield start : Intersection // The starting point of this Chute
- * @specfield startPort: integer // The port from which this exits its starting node
+ * @specfield startPort: integer // The port from which this exits its starting
+ * node
  * @specfield end : Intersection // The ending point of this Chute
- * @specfield endPort: integer // The port through which this enters its ending node
+ * @specfield endPort: integer // The port through which this enters its ending
+ * node
  * 
  * @specfield pinch : boolean // true iff there is a pinch-point in this chute
  * segment
  * 
  * @specfield narrow : boolean // true iff the chute is currently narrow
+ * 
+ * @specfield editable : boolean // true iff the player can edit the width of
+ * the chute
  * 
  * @specfield UID: integer // the unique odd identifier for this chute
  * 
@@ -26,8 +42,8 @@ package level;
 public class Chute
 {
    
-   private /*@LazyNonNull*/ Intersection start;
-   private /*@LazyNonNull*/ Intersection end;
+   private/* @LazyNonNull */Intersection start;
+   private/* @LazyNonNull */Intersection end;
    private final boolean pinch; // whether a chute has a pinch-point is a fact
                                 // of the original code, and cannot be modified
                                 // in-game
@@ -67,10 +83,17 @@ public class Chute
       throw new RuntimeException("Not yet implemented");
    }
    
+   public boolean isEditable()
+   {
+      throw new RuntimeException("Not yet implemented");
+   }
+   
+   // public void setEditable
+   
    /**
     * @return start, or null if end does not exist
     */
-   public /*@Nullable*/ Intersection getStart()
+   public/* @Nullable */Intersection getStart()
    {
       throw new RuntimeException("Not yet implemented");
    }
@@ -78,7 +101,7 @@ public class Chute
    /**
     * @return end, or null if end does not exist
     */
-   public /*@Nullable*/ Intersection getEnd()
+   public/* @Nullable */Intersection getEnd()
    {
       throw new RuntimeException("Not yet implemented");
    }
@@ -86,7 +109,8 @@ public class Chute
    /**
     * @requires start != null; port is a valid port number for start
     * @modifies this
-    * @effects sets "start" to the given Intersection, replacing the old one, if present
+    * @effects sets "start" to the given Intersection, replacing the old one, if
+    * present
     */
    protected void setStart(Intersection start, int port)
    {
@@ -96,7 +120,8 @@ public class Chute
    /**
     * @requires start != null; port is a valid port number for start
     * @modifies this
-    * @effects sets "end" to the given Intersection, replacing the old one, if present
+    * @effects sets "end" to the given Intersection, replacing the old one, if
+    * present
     */
    protected void setEnd(Intersection end, int port)
    {
--- java/Translation/src/levelBuilder/BoardBuilder.java
+++ java/Translation/src/levelBuilder/BoardBuilder.java
@@ -1,5 +1,9 @@
 package levelBuilder;
 
+import java.util.Set;
+
+import javax.lang.model.element.Name;
+
 import level.*;
 
 /**
@@ -15,7 +19,7 @@ import level.*;
  * Level that "board" belongs in -- stores information global to the level that
  * is needed for construction, but not gameplay
  * 
- * @specfield varToFurthestEdge: map from Name to Chute
+ * @specfield varToFurthestEdge: map from Name to Chute // For a given 
  * 
  * @specfield active: boolean // indicates whether this BoardBuilder represents
  * an unfinished Board
@@ -24,13 +28,52 @@ import level.*;
 
 public class BoardBuilder
 {
-	
-	/**
-	 * @effects creates a new BoardBuilder with the given LevelBuilder in the
-	 * levelBuilder specfield
-	 */
-	public BoardBuilder(LevelBuilder lb)
-	{
-		throw new RuntimeException("Not yet implemented");
-	}
+   
+   /**
+    * @effects creates a new BoardBuilder with the given LevelBuilder in the
+    * levelBuilder specfield
+    */
+   public BoardBuilder(LevelBuilder lb)
+   {
+      throw new RuntimeException("Not yet implemented");
+   }
+   
+   /**
+    * @requires The given node must have only a single input port and a single
+    * output port. Otherwise the state of the other ports would be undefined.
+    * @modifies this
+    * @effects Adds the given node to the given variable
+    */
+   public void addNodeToVar(Name var, Intersection node)
+   {
+      throw new RuntimeException("Not yet implemented");
+   }
+   
+   /**
+    * 
+    */
+   public void addVar(Name var)
+   {
+      throw new RuntimeException("Not yet implemented");
+   }
+   
+   /**
+    * @requires
+    */
+   public void addPinchToVar(Name var)
+   {
+      throw new RuntimeException("Not yet implemented");
+   }
+   
+   /**
+    * @requires bb's input and output ports all correspond to variables in this
+    * board
+    * @modifies this
+    * @effects adds the given sub-boards to this. For each of the chutes, there
+    * is a simple SPLIT at the top and a MERGE at the bottom.
+    */
+   public void addSubBoards(Set<BoardBuilder> bb)
+   {
+      throw new RuntimeException("Not yet implemented");
+   }
 }
