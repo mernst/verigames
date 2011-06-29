c4bef1911336ee5813d09af4b1f47bcd332fd95a
Nathaniel Mote <nmote@cs.washington.edu>
2011-06-29 13:24:58 -0700

    added some tests for Intersection

--- java/Translation/src/level/tests/IntersectionSpecTests.java
+++ java/Translation/src/level/tests/IntersectionSpecTests.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 import level.Chute;
@@ -22,7 +24,7 @@ import org.junit.Test;
 public class IntersectionSpecTests
 {
    // TODO write tests:
-   /* 
+   /*
     * test get__chute with existing chutes, non-existing chutes, and ports
     * higher than any given so far
     */
@@ -73,22 +75,60 @@ public class IntersectionSpecTests
    public Chute chute1;
    public Chute chute2;
    
+   public Method[] intersectionMethods;
+   
    @Before public void init()
    {
       i = new Intersection(Kind.INCOMING);
       j = new Intersection(Kind.OUTGOING);
       chute1 = new Chute(null, true, true, null);
       chute2 = new Chute(null, true, true, null);
+      
+      intersectionMethods = Intersection.class.getDeclaredMethods();
    }
    
-   @Test public void getInputChuteTest()
+   /**
+    * Tests that getInputChute returns the correct chute or null
+    */
+   @Test public void getInputChuteTest() throws IllegalAccessException,
+         InvocationTargetException
    {
-      assertNull(i.getInputChute(5));
-      // TODO finish
+      assertNull(j.getInputChute(5));
+      
+      for (Method m : intersectionMethods)
+      {
+         if (m.getName().equals("setInputChute"))
+         {
+            m.setAccessible(true);
+            Object[] args = { chute1, 5 };
+            m.invoke(j, args);
+         }
+      }
+      
+      assertEquals(j.getInputChute(5), chute1);
+      assertNull(j.getInputChute(1));
+      assertNull(j.getInputChute(7));
    }
    
-   @Test public void getOutputChuteTest()
+   /**
+    * Tests that getOutputChute returns the correect chute or null 
+    */
+   @Test public void getOutputChuteTest() throws IllegalAccessException, InvocationTargetException
    {
-      // TODO complete stub
+      assertNull(i.getOutputChute(5));
+      
+      for (Method m : intersectionMethods)
+      {
+         if (m.getName().equals("setOutputChute"))
+         {
+            m.setAccessible(true);
+            Object[] args = { chute1, 5 };
+            m.invoke(i, args);
+         }
+      }
+      
+      assertEquals(i.getOutputChute(5), chute1);
+      assertNull(i.getOutputChute(1));
+      assertNull(i.getOutputChute(7));
    }
 }
