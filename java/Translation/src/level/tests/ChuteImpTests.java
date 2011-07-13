169ee25bebbcd462515aff1225f9746ff629ba76
Nathaniel Mote <nmote@cs.washington.edu>
2011-07-13 11:26:45 -0700

    added tests for Chute for the 'active' property

--- java/Translation/src/level/tests/ChuteImpTests.java
+++ java/Translation/src/level/tests/ChuteImpTests.java
@@ -1,7 +1,14 @@
 package level.tests;
 
-import level.Chute;
+import static org.junit.Assert.fail;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import level.Chute;
+import level.Intersection;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class ChuteImpTests
@@ -23,5 +30,116 @@ public class ChuteImpTests
    @Test(expected = IllegalStateException.class) public void getEndPortTest()
    {
       (new Chute(null, true, true, null)).getEndPort();
+   }
+   
+   private Chute c;
+   
+   private Method[] ChuteMethods = Chute.class.getDeclaredMethods();
+   
+   /**
+    * Invokes a method with the given name on the given receiver, with the given
+    * arguments, subverting access control
+    */
+   private void invokeChuteMethod(Chute receiver, String name, Object[] args)
+         throws IllegalArgumentException, IllegalAccessException,
+         InvocationTargetException
+   {
+      boolean methodInvoked = false;
+      for (Method m : ChuteMethods)
+      {
+         if (m.getName().equals(name))
+         {
+            m.setAccessible(true);
+            m.invoke(c, args);
+            methodInvoked = true;
+         }
+      }
+      if (!methodInvoked)
+         throw new IllegalArgumentException("method " + name
+               + " does not exist");
+   }
+   
+   @Before public void initC() throws IllegalArgumentException,
+         IllegalAccessException, InvocationTargetException
+   {
+      c = new Chute("asdf", true, false, null);
+      
+      // c.setStart(Intersection.intersectionFactory(Intersection.Kind.INCOMING),0);
+      invokeChuteMethod(
+            c,
+            "setStart",
+            new Object[] {
+                  Intersection.intersectionFactory(Intersection.Kind.INCOMING),
+                  0 });
+      
+      // c.setEnd(Intersection.intersectionFactory(Intersection.Kind.OUTGOING));
+      invokeChuteMethod(
+            c,
+            "setEnd",
+            new Object[] {
+                  Intersection.intersectionFactory(Intersection.Kind.OUTGOING),
+                  0 });
+      
+      // c.deactivate();
+      invokeChuteMethod(c, "deactivate", new Object[] {});
+      
+   }
+   
+   @Test(expected = IllegalStateException.class) public void deactivateTest()
+         throws Throwable
+   {
+      // c.deactivate() (calling it a second time should throw an
+      // IllegalStateException)
+      try
+      {
+         invokeChuteMethod(c, "deactivate", new Object[] {});
+      }
+      catch (InvocationTargetException e)
+      {
+         throw e.getCause();
+      }
+      catch (Exception e)
+      {
+         fail();
+      }
+   }
+   
+   @Test(expected = IllegalStateException.class) public void narrowTest()
+   {
+      c.setNarrow(true);
+   }
+   
+   @Test(expected = IllegalStateException.class) public void setStartTest() throws Throwable
+   {
+      // c.setStart(Intersection.intersectionFactory(Intersection.Kind.START_WHITE_BALL),0);
+      try
+      {
+         invokeChuteMethod(c, "setStart", new Object[] {Intersection.intersectionFactory(Intersection.Kind.START_WHITE_BALL), 0});
+      }
+      catch (InvocationTargetException e)
+      {
+         throw e.getCause();
+      }
+      catch (Exception e)
+      {
+         fail();
+      }
+   }
+   
+   @Test(expected = IllegalStateException.class) public void setEndTest() throws Throwable
+   {
+      // c.setEnd(Intersection.intersectionFactory(Intersection.Kind.END),0);
+      try
+      {
+         invokeChuteMethod(c, "setEnd", new Object[] {Intersection.intersectionFactory(Intersection.Kind.END), 0});
+      }
+      catch (InvocationTargetException e)
+      {
+         throw e.getCause();
+      }
+      catch (Exception e)
+      {
+         fail();
+      }
    }
 }
