fbf477057f9a35f84bbf274cc8db21305f6b96aa
Nathaniel Mote <nmote@cs.washington.edu>
2011-07-20 13:55:27 -0700

    added some tests for LevelBuilder and BoardBuilder

--- java/Translation/src/levelBuilder/tests/LevelBuilderSpecTests.java
+++ java/Translation/src/levelBuilder/tests/LevelBuilderSpecTests.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import level.Chute;
 import level.Intersection.Kind;
@@ -34,7 +36,7 @@ public class LevelBuilderSpecTests
     * }
     */
    @Test
-   public void generateBasicXML() throws FileNotFoundException
+   public void generateTestClassXML() throws FileNotFoundException
    {
       LevelBuilder TestClass = new LevelBuilder();
       
@@ -58,5 +60,56 @@ public class LevelBuilderSpecTests
       w.outputXML(out);
       
       out.close();
+   }
+   
+   /**
+    * Generates the XML for TestClass2 (below)
+    * 
+    * class TestClass2
+    * {
+    *    List<String> list;
+    *    
+    *    public TestClass2()
+    *    {
+    *       list = new List<String>();
+    *    }
+    *    
+    *    public addElt()
+    *    {
+    *       list.add("asdf");
+    *    }
+    * }
+    */
+   @Test
+   public void generateTestClass2XML() throws FileNotFoundException
+   {
+      LevelBuilder TestClass2 = new LevelBuilder();
+      
+      List<Chute> auxChutes = new ArrayList<Chute>();
+      auxChutes.add(new Chute(null, true, null));
+      
+      TestClass2.addField(new Chute("list", true, auxChutes));
+      
+      BoardBuilder constructor = TestClass2.getConstructorTemplate("constructor");
+      constructor.assignment("list", Kind.START_WHITE_BALL);
+      
+      TestClass2.finishBoardBuilder(constructor);
+      
+      BoardBuilder addElt = TestClass2.getTemplateBoardBuilder("addElt");
+      addElt.addPinchToVar("list");
+      
+      // merge into aux chute somehow
+      
+      TestClass2.finishBoardBuilder(addElt);
+      
+      World w = new World();
+      w.add(TestClass2.getLevel());
+      
+      PrintStream out = new PrintStream(new FileOutputStream(new File("TestClass2.actual.xml")));
+      
+      w.outputXML(out);
+      
+      out.close();
+      
    }
 }
