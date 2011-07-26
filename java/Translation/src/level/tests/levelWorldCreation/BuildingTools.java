574c07d13346dc63eea31ec3721344dca8e137bc
Nathaniel Mote <nmote@cs.washington.edu>
2011-07-26 11:18:18 -0700

    added convenience method to BuildingTools

--- java/Translation/src/level/tests/levelWorldCreation/BuildingTools.java
+++ java/Translation/src/level/tests/levelWorldCreation/BuildingTools.java
@@ -6,10 +6,28 @@ import java.util.Map;
 
 import level.Board;
 import level.Chute;
+import level.Intersection;
+import level.Intersection.Kind;
 import level.Level;
 
 public class BuildingTools
 {
+   
+   /**
+    * Makes a new Board, adds it to level with the given name, adds incoming and
+    * outgoing nodes to it.
+    */
+   protected static Board initializeBoard(Level level, String name)
+   {
+      Board b = new Board();
+      level.addBoard(name, b);
+      
+      b.addNode(Intersection.factory(Kind.INCOMING));
+      b.addNode(Intersection.factory(Kind.OUTGOING));
+      
+      return b;
+   }
+   
    protected static void connectFields(Board b, Level level, Map<String, Chute> fieldToChute, Map<String, Integer> nameToPort, String... fieldNames)
    {  
       for (String name : fieldNames)
