845e902e1832ba8c7198ee0dc3e1644f73104176
Nathaniel Mote <nmote@cs.washington.edu>
2011-09-08 23:04:44 -0700

    added tests

---
+++ java/Translation/src/layout/tests/DotParserSpecTests.java
@@ -0,0 +1,61 @@
+package layout.tests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import layout.GraphInformation;
+import layout.DotParser;
+
+public class DotParserSpecTests
+{
+   private static final String testInput =
+      "digraph {"
+      + "	graph [nodesep=0, ranksep=0];"
+      + "	node [label=\"\\N\", shape=box, fixedsize=true];"
+      + "	graph [bb=\"0,0,216.69,528\"];"
+      + "	8 [label=INCOMING8, width=2, height=2, pos=\"101.64,456\"];"
+      + "	9 [label=OUTGOING9, width=1, height=1, pos=\"129.64,36\"];"
+      + "	10 [label=END10, width=1, height=2, pos=\"82.635,304\"];"
+      + "	11 [label=MERGE11, width=2, height=2, pos=\"129.64,152\"];"
+      + "	12 [label=START_BLACK_BALL12, width=1, height=2, pos=\"158.64,304\"];"
+      + "	8 -> 10 [pos=\"e,91.662,376.22 92.58,383.56 92.561,383.41 92.542,383.26 92.523,383.1\"];"
+      + "	8 -> 11 [pos=\"e,126,224.05 114.66,383.67 115.01,381.08 115.34,378.52 115.64,376 118.79,349.38 122.59,286.68 125.44,234.36\"];"
+      + "	11 -> 9 [pos=\"e,129.64,72.256 129.64,79.855 129.64,79.693 129.64,79.532 129.64,79.371\"];"
+      + "	12 -> 11 [pos=\"e,143.41,224.22 144.81,231.56 144.79,231.41 144.76,231.26 144.73,231.1\"];"
+      + "	8 -> 9 [style=invis, weight=0, pos=\"e,93.412,44.093 48.526,383.68 47.463,381.14 46.493,378.57 45.635,376 4.021,251.18 -28.789,191.77 40.635,80 50.291,64.456 67.244,\\"
+      + "54.237 83.782,47.6\"];"
+      + "	8 -> 10 [style=invis, weight=0, pos=\"e,92.411,376.22 93.33,383.56 93.311,383.41 93.292,383.26 93.272,383.1\"];"
+      + "	10 -> 9 [style=invis, weight=0, pos=\"e,93.298,48.781 53.201,231.76 39.261,185.57 30.689,125.93 56.635,80 62.952,68.817 73.377,60.159 84.399,53.6\"];"
+      + "	8 -> 11 [style=invis, weight=0, pos=\"e,126.81,224.05 115.66,383.67 116.01,381.08 116.34,378.52 116.64,376 119.79,349.38 123.59,286.68 126.29,234.36\"];"
+      + "	11 -> 9 [style=invis, weight=0, pos=\"e,130.34,72.256 130.37,79.855 130.36,79.693 130.36,79.532 130.36,79.371\"];"
+      + "	8 -> 12 [style=invis, weight=0, pos=\"e,131.55,376.22 128.8,383.56 128.86,383.41 128.91,383.26 128.97,383.1\"];"
+      + "	12 -> 9 [style=invis, weight=0, pos=\"e,165.85,50.213 194.77,237.19 196.29,232.8 197.6,228.38 198.64,224 213.34,161.71 229.41,136.12 198.64,80 193.09,69.883 184.12,61.757\\"
+      + " 174.45,55.377\"];"
+      + "}";
+
+      private static final GraphInformation testOutput;
+      static
+      {
+         GraphInformation.Builder builder = new GraphInformation.Builder();
+
+         builder.setGraphAttributes(new GraphInformation.GraphAttributes(21700, 52800));
+
+         builder.setNodeAttributes("8", new GraphInformation.NodeAttributes(10164, 45600, 14400, 7200));
+         builder.setNodeAttributes("9", new GraphInformation.NodeAttributes(12964, 3600, 7200, 7200));
+         builder.setNodeAttributes("10", new GraphInformation.NodeAttributes(8264, 30400, 7200, 14400));
+         builder.setNodeAttributes("11", new GraphInformation.NodeAttributes(12964, 15200, 14400, 14400));
+         builder.setNodeAttributes("12", new GraphInformation.NodeAttributes(15864, 30400, 7200, 14400));
+
+         testOutput = builder.build();
+      }
+
+      /**
+       * Tests that the parser gives testOutput on testInput
+       */
+      @Test
+      public void simpleTest()
+      {
+         DotParser parser = new DotParser();
+         assertEquals(testOutput, parser.parse(testInput));
+      }
+}
