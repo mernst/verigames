aeaecd2b1b67c11bec08859a447df1cc62907156
Nathaniel Mote <nmote@cs.washington.edu>
2011-10-02 11:48:01 -0700

    added package-info.java file to utilities package

---
+++ java/Translation/src/utilities/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides utilities used throughout the verigames packages. The contents of
+ * this package are not to be used by clients, and their specifications are not
+ * guaranteed to remain constant.
+ */
+
+package utilities;
