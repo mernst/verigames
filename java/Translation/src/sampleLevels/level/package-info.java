f9ae0474ee421df777364a13a0a853eeb3349eea
Nathaniel Mote <nmote@cs.washington.edu>
2011-10-15 23:05:40 -0700

    added package description for the package that generates a world based on the level package

---
+++ java/Translation/src/sampleLevels/level/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains code that generates a {@link level.World World} hand-translated from the source in the {@link level} package.
+ * <p>
+ * Based on the code at changeset 594f3ec9f9d4
+ */
+
+package sampleLevels.level;
