5cacdf5806958e3bd8975b99c44a0d3a6910e954
Nathaniel Mote <nmote@cs.washington.edu>
2011-11-13 20:57:43 -0800

    overrode equals and hashCode in Pair

--- java/Translation/src/utilities/Pair.java
+++ java/Translation/src/utilities/Pair.java
@@ -2,6 +2,9 @@ package utilities;
 
 /**
  * An immutable record type representing a pair of arbitrary elements.
+ * <p>
+ * A pair is equal to another object if and only if the other object is also a
+ * pair, and if the first elements are equal and the second elements are equal.
  */
 public class Pair<A,B>
 {
@@ -22,5 +25,24 @@ public class Pair<A,B>
    public B getSecond()
    {
       return second;
+   }
+
+   @Override
+   public boolean equals(Object o)
+   {
+      if (o instanceof Pair)
+      {
+         Pair<?,?> other = (Pair<?,?>) o;
+         return this.getFirst().equals(other.getFirst()) &&
+               this.getSecond().equals(other.getSecond());
+      }
+      else
+         return false;
+   }
+
+   @Override
+   public int hashCode()
+   {
+      return first.hashCode() * 31 + second.hashCode() * 13;
    }
 }
