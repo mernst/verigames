a714c89823dca1f89ff567e89333258ce02d5d4f
Nathaniel Mote <nmote@cs.washington.edu>
2011-12-03 11:43:20 -0800

    removed failed LevelBuilder experiment

--- java/Translation/src/levelBuilder/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Contains structures that allow the creation of verification game levels using
- * higher level commands, reducing the need for clients to interact with the low
- * level details of level construction.
- */
-
-package levelBuilder;
