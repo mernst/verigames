40ec011d4e88278e85f2ae1e61e3d156b6a8db9b
Nathaniel Mote <nmote@cs.washington.edu>
2012-01-29 20:28:51 -0800

    added information about Level.makeLinked to level package-info

--- java/Translation/src/verigames/level/package-info.java
+++ java/Translation/src/verigames/level/package-info.java
@@ -8,19 +8,20 @@
  * considered. For example, it may make sense to remove the one-to-one relation
  * between {@code Board}s and Java methods.
  * <p>
- * A {@link verigames.level.World World} represents a single game for Pipe Jam. No two
- * Worlds can be dependent on each other; any two worlds can be solved in
+ * A {@link verigames.level.World World} represents a single game for Pipe Jam.
+ * No two Worlds can be dependent on each other; any two worlds can be solved in
  * isolation. A {@code World} represents, typically, one Java project. 
  * <p>
- * Each {@code World} contains an arbitrary number of {@link verigames.level.Level
- * Level}s. {@code Level}s can be dependent on one another. Each level
- * represents a class in a Java project.
+ * Each {@code World} contains an arbitrary number of {@link
+ * verigames.level.Level Level}s. {@code Level}s can be dependent on one
+ * another. Each level represents a class in a Java project.
  * <p>
- * Each {@code Level} contains an arbitrary number of {@link verigames.level.Board}s.
- * {@code Board}s can be dependent both on other {@code Board}s in the same
- * {@code Level} and on {@code Board}s in other {@code Level}s in the same
- * {@code World}. A {@code Board} represents a method. A {@code Board} is
- * essentially a DAG with {@link verigames.level.Chute Chute}s as edges and {@link
+ * Each {@code Level} contains an arbitrary number of {@link
+ * verigames.level.Board}s.  {@code Board}s can be dependent both on other
+ * {@code Board}s in the same {@code Level} and on {@code Board}s in other
+ * {@code Level}s in the same {@code World}. A {@code Board} represents a
+ * method. A {@code Board} is essentially a DAG with {@link
+ * verigames.level.Chute Chute}s as edges and {@link
  * verigames.level.Intersection Intersection}s.
  * <p>
  * A {@code Chute}, loosely, represents a type. The term "loosely" is used
@@ -30,8 +31,8 @@
  * <p>
  * An {@code Intersection} is the terminating point for a {@code Chute}.
  * Typically, it is no more than that, though in the cases of {@link
- * verigames.level.Subnetwork Subnetwork} and {@link verigames.level.NullTest NullTest}, more
- * meaning is carried.
+ * verigames.level.Subnetwork Subnetwork} and {@link verigames.level.NullTest
+ * NullTest}, more meaning is carried.
  * <p>
  * Every {@code Board} has two special {@code Intersection}s: The incoming one
  * and the outgoing one.
@@ -46,6 +47,13 @@
  * method should be attached to incoming and outgoing nodes. Chutes representing
  * the types of local variables should be started with a START_XXX_BALL node and
  * ended with an END node.
+ * <p>
+ * In order to fully express the constraints in the game, some chutes must
+ * represent the same type. This means that if one of these chutes changes
+ * width, all the others representing the same variable must too. To express
+ * this information, a client should call the {@link
+ * verigames.level.Level#makeLinked Level.makeLinked} method with these {@code
+ * Chute}s.
  */
 
 package verigames.level;
