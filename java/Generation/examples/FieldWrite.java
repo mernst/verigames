608f44c9d4a071dfb06991f35b559e99b6cb70a8
Werner Dietl <wdietl@gmail.com>
2012-03-02 04:21:25 -0800

    Update examples.

--- java/Generation/examples/FieldWrite.java
+++ java/Generation/examples/FieldWrite.java
@@ -9,3 +9,23 @@ class FieldWrite {
     f = null;
   }
 }
+
+class FWOther {
+  FieldWrite fw;
+
+  void write1() {
+    fw.f = new Object();
+    this.fw.f = null;
+  }
+
+  void read1() {
+    Object x;
+    x = fw.f;
+    x = fw.f;
+  }
+
+  void write2(FieldWrite p) {
+    p.f = new Object();
+    p.f = null;
+  }
+}
--- java/Generation/examples/StaticInit.java
+++ java/Generation/examples/StaticInit.java
@@ -1,7 +1,9 @@
 class StaticInit {
+/* TODO: Static field access not handled yet!
   static Object f;
 
   static {
     f = new Object();
   }
+*/
 }
