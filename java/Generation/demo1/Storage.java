5b57a3cd08aa6d297cb38ce01adf71122f908da3
Werner Dietl <wdietl@gmail.com>
2012-03-05 13:08:11 -0800

    Make the getter method a bit more complicated to force the use of buzzsaws.

--- java/Generation/demo1/Storage.java
+++ java/Generation/demo1/Storage.java
@@ -2,7 +2,14 @@ class Storage {
     Object data;
 
     Object get() {
-        return data;
+        Object res;
+        if (data!=null && (data.hashCode()%5 == 0)) {
+            // This is valid data.
+            res = data;
+        } else {
+            res = null;
+        }
+        return res;
     }
 
     void set(Object p) {
