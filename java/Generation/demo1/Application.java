df65a09d9d60cc3e5c8ff8075b539fcd344707bc
Werner Dietl <wdietl@gmail.com>
2012-03-04 18:52:19 -0800

    Simplify the example. Together with Application2 this should now require a buzzsaw.

--- java/Generation/demo1/Application.java
+++ java/Generation/demo1/Application.java
@@ -1,12 +1,9 @@
-class Application {
+class Application1 {
     void main() {
-        Scribble sc = new Scribble();
-        sc.init();
-
-        Server s = new Server(99);
-        s.run();
-
         Storage store = new Storage();
         store.set(new UserData("Demo Name", 44));
+
+        Object ud = store.get();
+        ud.toString();
     }
 }
