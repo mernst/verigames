7a042c55a39b2c9056363c9864d7d109d57386a5
Nathaniel Mote <nmote@cs.washington.edu>
2012-04-07 14:06:57 -0700

    added test for linked chute width consistency checking

---
+++ java/Translation/test/verigames/level/LevelImpTests.java
@@ -0,0 +1,58 @@
+package verigames.level;
+
+import static verigames.level.Intersection.Kind.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LevelImpTests
+{
+  Level l = null;
+  Chute c1 = null;
+  Chute c2 = null;
+
+  @Before
+  public void init()
+  {
+    l = new Level();
+    Board b = new Board();
+    
+    Intersection incoming = Intersection.factory(INCOMING);
+    Intersection outgoing = Intersection.factory(OUTGOING);
+
+    b.addNode(incoming);
+    b.addNode(outgoing);
+
+    c1 = new Chute();
+    c2 = new Chute();
+
+    b.addEdge(incoming, 0, outgoing, 0, c1);
+    b.addEdge(incoming, 1, outgoing, 1, c2);
+
+    l.makeLinked(c1,c2);
+  }
+
+  /*
+   * Test that when construction is finished, Level throws an exception if
+   * linked {@link Chute}s have differing widths.
+   */
+  @Test(expected = IllegalStateException.class)
+  public void testWidthConsistencyChecking1()
+  {
+    c1.setNarrow(false);
+    c2.setNarrow(true);
+    l.finishConstruction();
+  }
+
+  /*
+   * Make sure that in the previous test, the {@code IllegalStateException}
+   * isn't thrown for some other reason
+   */
+  @Test
+  public void testWidthConsistencyChecking2()
+  {
+    c1.setNarrow(true);
+    c2.setNarrow(true);
+    l.finishConstruction();
+  }
+}
