3cd3790c35c5230de57680276c6772075faa22fd
Nathaniel Mote <nmote@cs.washington.edu>
2012-04-18 01:04:51 -0700

    added entry points for generating sample level XML

---
+++ java/Translation/src/verigames/sampleLevels/intro/GenerateXML.java
@@ -0,0 +1,23 @@
+package verigames.sampleLevels.intro;
+
+import verigames.layout.WorldLayout;
+import verigames.level.World;
+import verigames.level.WorldXMLPrinter;
+
+/**
+ * Provides a top-level application that generates the XML for the intro World
+ * and prints it to standard out.
+ *
+ * @author Nathaniel Mote
+ */
+
+public class GenerateXML
+{
+  public static void main(String[] args)
+  {
+    World w = IntroWorld.getWorld();
+    WorldLayout.layout(w);
+
+    (new WorldXMLPrinter()).print(w, System.out, null);
+  }
+}
