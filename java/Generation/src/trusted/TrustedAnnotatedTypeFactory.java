b804eb3b913e762ce6f5eb0a3136530e0e558137
Werner Dietl <wdietl@gmail.com>
2012-04-23 04:30:12 -0700

    Use flow by using the BasicATF.

--- java/Generation/src/trusted/TrustedAnnotatedTypeFactory.java
+++ java/Generation/src/trusted/TrustedAnnotatedTypeFactory.java
@@ -1,45 +1,13 @@
 package trusted;
 
-import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
-
-import checkers.types.AnnotatedTypeFactory;
-import checkers.types.AnnotatedTypeMirror;
+import checkers.types.BasicAnnotatedTypeFactory;
 
 import com.sun.source.tree.CompilationUnitTree;
-import com.sun.source.tree.Tree;
 
-public class TrustedAnnotatedTypeFactory extends AnnotatedTypeFactory {
-    TrustedChecker checker;
+public class TrustedAnnotatedTypeFactory extends BasicAnnotatedTypeFactory<TrustedChecker> {
 
     public TrustedAnnotatedTypeFactory(TrustedChecker checker,
             CompilationUnitTree root) {
         super(checker, root);
-
-        this.checker = checker;
-
-        postInit();
-    }
-
-    @Override
-    protected void annotateImplicit(Tree tree, AnnotatedTypeMirror type) {
-    	System.out.println("Tree: " + tree.getKind() + ", class: " + tree.toString());
-        if (!type.isAnnotated()) {
-            System.out.println("Tree without annotation (trusted2): " + tree.getKind());
-            
-            if (tree.getKind() == Tree.Kind.STRING_LITERAL) {
-                type.addAnnotation(checker.TRUSTED);
-                System.out.println("Annotating String: " + tree.toString() + " as trusted.");
-            }
-        }
-    }
-
-    @Override
-    protected void annotateImplicit(Element elt, AnnotatedTypeMirror type) {
-        if (!type.isAnnotated()) {
-            // System.out.println("Element without annotation: " +
-            // elt.getKind());
-            // type.addAnnotation(checker.NULLABLE);
-        }
     }
 }
