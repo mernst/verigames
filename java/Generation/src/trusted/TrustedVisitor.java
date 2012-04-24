439eda580ecdeae81247f0ca994bde6d6dd824f1
Werner Dietl <wdietl@gmail.com>
2012-04-23 21:05:00 -0700

    Log method invocations.

--- java/Generation/src/trusted/TrustedVisitor.java
+++ java/Generation/src/trusted/TrustedVisitor.java
@@ -1,7 +1,5 @@
 package trusted;
 
-import javax.lang.model.type.TypeMirror;
-
 import checkers.basetype.BaseTypeChecker;
 import checkers.inference.InferenceVisitor;
 
@@ -9,14 +7,17 @@ import com.sun.source.tree.*;
 
 public class TrustedVisitor extends InferenceVisitor {
 
-    private final TrustedChecker trustedchecker;
-    private final TypeMirror stringType;
-
     public TrustedVisitor(BaseTypeChecker checker, CompilationUnitTree root,
             TrustedChecker trustedchecker, boolean infer) {
         super(checker, root, infer);
+    }
 
-        this.trustedchecker = trustedchecker;
-        this.stringType = elements.getTypeElement("java.lang.String").asType();
+    /** Log method invocations. */
+    // TODO: Add a GameVisitor that does the field/method logging.
+    @Override
+    public Void visitMethodInvocation(MethodInvocationTree node, Void p) {
+        logMethodInvocation(node);
+        super.visitMethodInvocation(node, p);
+        return null;
     }
 }
