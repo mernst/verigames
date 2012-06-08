49d541db43425224b4599d749ef30371cc7c7919
Brian Walker <bdwalker@cs.washington.edu>
2012-06-08 12:54:26 -0700

    Updating scripts folder to most recent published files.

--- website/scripts/footer.php
+++
@@ -1,6 +0,0 @@
-<?php?>
-	<div id="footer">
-		&copy; Department of Computer Science and Engineering, University of Washington, Seattle
-	</div>
-	</body>
-</html>
--- website/scripts/retrieve_file_contents.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file_path = $_REQUEST["path"];
-chdir("/homes/abstract/bdwalker/www/live/uploads/");
-if(file_exists('./'.$file_path)){
-	$contents = file_get_contents('./'.$file_path);
-	print($contents);
-}
-?>
