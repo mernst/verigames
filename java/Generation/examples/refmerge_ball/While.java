7d3d6ee93e10b15ad927b4931701a8423a11bd68
David McArthur <mcarthur@cs.washington.edu>
2013-09-20 14:42:02 -0700

    Adding For tests. Updated while test.

--- java/Generation/examples/refmerge_ball/While.java
+++ java/Generation/examples/refmerge_ball/While.java
@@ -2,21 +2,11 @@ class While {
     void test(boolean b) {
        String a = "";
        while (b) {
-            a.toString();
-            if (a != null) {
-                a = null;
-            }
+           if (a == null) {
+               a.toString();
+           } else {
+               a = null;
+           }
        }
-
-/*       String a = ""; // 4
-        while (b) { // 6 + 4 => 7, 7 + 4 => 7
-            a.toString(); // 4, 7
-            if (b) {
-                a = "" // 5
-            }
-            // 4 + 5 => 6
-            // 5 + 7 => 7
-            a.toString() // 6
-        }*/
     }
 }
