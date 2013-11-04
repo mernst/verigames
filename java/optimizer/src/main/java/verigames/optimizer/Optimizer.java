d843bcd938ef228c500b16bdf90bd06dfac7d4bf
Calvin Loncaric <loncaric@cs.washington.edu>
2013-11-04 08:55:55 -0800

    Remove some dead code from the optimizer

--- java/optimizer/src/main/java/verigames/optimizer/Optimizer.java
+++ java/optimizer/src/main/java/verigames/optimizer/Optimizer.java
@@ -5,34 +5,15 @@ import verigames.level.Intersection;
 import verigames.level.World;
 import verigames.optimizer.model.Node;
 import verigames.optimizer.model.NodeGraph;
-import verigames.optimizer.model.Port;
 import verigames.optimizer.model.Subgraph;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 public class Optimizer {
 
     public void optimize(NodeGraph g) {
         removeIsolatedComponents(g);
         compressConnectors(g);
-
-        // TODO: remove all small ball drops?
-//        Collection<Node> smallBallStarts = new ArrayList<>();
-//        for (Node node : g.getNodes()) {
-//            if (node.getIntersection().getIntersectionKind() == Intersection.Kind.START_SMALL_BALL) {
-//                smallBallStarts.add(node);
-//            }
-//        }
-//
-//        for (Node node : smallBallStarts) {
-//            Collection<Node> toRemove = removeSource(g, node);
-//            if (toRemove != null) {
-//                g.removeNodes(toRemove);
-//            }
-//        }
     }
 
     /**
@@ -119,30 +100,6 @@ public class Optimizer {
                         newChute);
             }
         }
-    }
-
-    private Set<Node> removeSource(NodeGraph g, Node node) {
-        Set<Node> toRemove = new HashSet<>();
-        toRemove.add(node);
-        // for each outgoing edge
-        for (Map.Entry<Port, NodeGraph.Target> edge : g.outgoingEdges(node).entrySet()) {
-            Port port = edge.getKey();
-            NodeGraph.Target target = edge.getValue();
-            if (target.getDst().getIntersection().getIntersectionKind().getNumberOfInputPorts() == 1) {
-                // EITHER the edge connects to a single-input node which we can also remove
-                Set<Node> subNodes = removeSource(g, target.getDst());
-                if (subNodes != null)
-                    toRemove.addAll(subNodes);
-                else
-                    return null;
-            } else if (target.getDst().getIntersection().getIntersectionKind().getNumberOfInputPorts() < 0) {
-                // OR it connects to a variable-input node and we can remove the edge
-                // TODO: remove the edge
-            } else {
-                return null;
-            }
-        }
-        return toRemove;
     }
 
     public World optimizeWorld(World source) {
