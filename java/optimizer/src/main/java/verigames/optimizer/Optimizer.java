c795516bcd8b48cb43caeb64415f000356d6a3cd
Calvin Loncaric <loncaric@cs.washington.edu>
2013-11-05 14:44:56 -0800

    Small improvement to the split-elimination optimization pass

--- java/optimizer/src/main/java/verigames/optimizer/Optimizer.java
+++ java/optimizer/src/main/java/verigames/optimizer/Optimizer.java
@@ -24,13 +24,28 @@ public class Optimizer {
             new ConnectorCompression(),
             new ImmutableComponentElimination());
 
+    public static final int DEFAULT_MAX_ITERATIONS = 20;
+
     public void optimize(NodeGraph g) {
         optimize(g, DEFAULT_PASSES);
     }
 
     public void optimize(NodeGraph g, List<OptimizationPass> passes) {
-        for (OptimizationPass pass : passes) {
-            pass.optimize(g);
+        optimize(g, passes, DEFAULT_MAX_ITERATIONS);
+    }
+
+    public void optimize(NodeGraph g, List<OptimizationPass> passes, int maxIterations) {
+        int nodes = g.getNodes().size();
+        int edges = g.getEdges().size();
+        for (int i = 0; i < maxIterations; ++i) {
+            for (OptimizationPass pass : passes) {
+                pass.optimize(g);
+            }
+            // stop early when we stop making progress
+            if (g.getNodes().size() >= nodes && g.getEdges().size() >= edges)
+                break;
+            nodes = g.getNodes().size();
+            edges = g.getEdges().size();
         }
     }
 
--- java/optimizer/src/main/java/verigames/optimizer/passes/SplitElimination.java
+++ java/optimizer/src/main/java/verigames/optimizer/passes/SplitElimination.java
@@ -25,7 +25,7 @@ import java.util.List;
  *
  * Where "^" is a split node, "O" is a connector, and the chute connecting to
  * the END node is either (1) immutable wide or (2) mutable and the sole
- * member of its edge set.
+ * member of its edge set or (3) in the same edge set as the incoming edge.
  *
  * This happens occasionally in real worlds, but is also a relatively common
  * output case from the {@link ChuteEndElimination} optimization pass.
@@ -52,15 +52,21 @@ public class SplitElimination implements OptimizationPass {
                 Intersection i = Intersection.factory(Intersection.Kind.CONNECT);
                 Node connector = new Node(levelName, level, boardName, board, i);
 
+                int e1VarID = e1.getEdgeData().getVariableID();
+                int e2VarId = e2.getEdgeData().getVariableID();
+                int srcVarId = src.getEdgeData().getVariableID();
+
                 // If either node was a small ball drop, we need to remove this
                 // merge and the drop and place a connector instead.
-                if (e1.getDst().getIntersection().getIntersectionKind() == Intersection.Kind.END && Util.conflictFree(g, e1)) {
+                if (e1.getDst().getIntersection().getIntersectionKind() == Intersection.Kind.END &&
+                        (Util.conflictFree(g, e1) || e1VarID == srcVarId)) {
                     g.removeNode(e1.getDst());
                     g.removeNode(n);
                     g.addNode(connector);
                     g.addEdge(src.getSrc(), src.getSrcPort(), connector, Port.INPUT, e2.getEdgeData());
                     g.addEdge(connector, Port.OUTPUT, e2.getDst(), e2.getDstPort(), e2.getEdgeData());
-                } else if (e2.getDst().getIntersection().getIntersectionKind() == Intersection.Kind.END && Util.conflictFree(g, e2)) {
+                } else if (e2.getDst().getIntersection().getIntersectionKind() == Intersection.Kind.END &&
+                        (Util.conflictFree(g, e2) || e2VarId == srcVarId)) {
                     g.removeNode(e2.getDst());
                     g.removeNode(n);
                     g.addNode(connector);
