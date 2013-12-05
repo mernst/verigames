a12c0e21462e832b007d8184c1b9cede98ddcfca
Calvin Loncaric <loncaric@cs.washington.edu>
2013-12-05 13:26:52 -0800

    Fix an incorrect comment

--- java/optimizer/src/main/java/verigames/optimizer/model/MismatchException.java
+++ java/optimizer/src/main/java/verigames/optimizer/model/MismatchException.java
@@ -1,7 +1,7 @@
 package verigames.optimizer.model;
 
 /**
- * Thrown by {@link ReverseMapping#apply(verigames.level.World, verigames.level.World)}
+ * Thrown by {@link ReverseMapping#solutionForUnoptimized(NodeGraph, NodeGraph, Solution)}
  * when the mapping does not seem to describe the given worlds.
  */
 public class MismatchException extends Exception {
