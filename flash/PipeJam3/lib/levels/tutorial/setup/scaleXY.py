1132f1cf6a9d402d837e98486046b175adf00073
Seth Cooper <seth.cooper@gmail.com>
2016-06-21 10:40:57 -0400

    Update level scaling script.

--- flash/PipeJam3/lib/levels/tutorial/setup/scaleXY.py
+++ flash/PipeJam3/lib/levels/tutorial/setup/scaleXY.py
@@ -1,22 +1,35 @@
 import sys, json
 
 if len(sys.argv) != 3:
-	print 'Usage: %s filename scale_factor_float' % argv[0]
+	print 'Usage: %s filename scale_factor_float' % sys.argv[0]
 	quit()
-scale_factor = float(sys.argv[2])
+
 layout_f = sys.argv[1]
+scale_factor = float(sys.argv[2])
+
+lo_x = lo_y = 1e100
+hi_x = hi_y = -1e100
+
 with open(layout_f, 'r') as fin:
 	layout_obj = json.load(fin)
-	for group_key in layout_obj.get('layout', {}):
-		for key in layout_obj['layout'][group_key]:
-			thing = layout_obj['layout'][group_key][key]
-			try:
-				thing['x'] = float(thing['x']) * scale_factor
-			except Exception as e:
-				pass
-			try:
-				thing['y'] = float(thing['y']) * scale_factor
-			except Exception as e:
-				pass
+	for node_id in layout_obj['layout']['vars']:
+		coords = layout_obj['layout']['vars'][node_id]
+		try:
+                        new_x = float(coords['x']) * scale_factor
+			coords['x'] = new_x
+                        lo_x = min(lo_x, new_x)
+                        hi_x = max(hi_x, new_x)
+		except Exception as e:
+			pass
+		try:
+                        new_y = float(coords['y']) * scale_factor
+			coords['y'] = new_y
+                        lo_y = min(lo_y, new_y)
+                        hi_y = max(hi_y, new_y)
+		except Exception as e:
+			pass
+        if layout_obj['layout'].has_key('bounds'):
+                layout_obj['layout']['bounds'] = [lo_x, lo_y, hi_x, hi_y]
+
 with open(layout_f, 'w') as fout:
 	fout.write(json.dumps(layout_obj, indent=2, sort_keys=True))
