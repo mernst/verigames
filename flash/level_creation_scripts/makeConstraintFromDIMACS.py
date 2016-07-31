f68ee71e0f6315fa0192950daf44f63ca225a315
Seth Cooper <seth.cooper@gmail.com>
2016-07-31 15:19:07 -0400

    Maintain comments from DIMACS.

--- flash/level_creation_scripts/makeConstraintFromDIMACS.py
+++ flash/level_creation_scripts/makeConstraintFromDIMACS.py
@@ -5,14 +5,14 @@ def initInfo():
 
 # if a line starts with p, check that it contains 'cnf'
 def readPLine(vals):
-    if("cnf" not in vals):
+    if('cnf' not in vals):
         print 'Cannot handle formats other than cnf'
         quit()
         
 def addListItems(list):
-    string = ""
+    string = ''
     for i in range (0, len(list)):
-        if list[i] == "0":
+        if list[i] == '0':
             break;
         else:
             string += '"var:' + list[i] + '", '
@@ -28,46 +28,64 @@ with open(sys.argv[1]) as f:
     content = f.readlines()
     
 result = initInfo()
+comments = []
 
 for line in content:
     # get the individual items in the line as a list
     values = line.split()
-    
-    # if the value isn't a comment
-    if not (values[0] == "c"):
+
+    if len(values) == 0:
+        continue
+
+    if values[0] == 'c':
+        # remember comments
+        comments.append(line[1:].strip())
+
+    elif values[0] == 'p':
         # check if the line starts with a p
-        if (values[0] == "p"):
-            readPLine(values)
-        else:
+        readPLine(values)
+
+    else:
+        # the first line of each
+        result += '\t\t{\n\t\t\t"constraint": "clause",\n'
             
-            # the first line of each
-            result += '\t\t{\n\t\t\t"constraint": "clause",\n'
+        pos = []
+        neg = []
+
+        # for each item in the list of items
+        for i in range (0, len(values)):
+            if (len(values) == 2):
+                print 'Invalid format'
+                quit()
             
-            pos = []
-            neg = []
-            # for each item in the list of items
-            for i in range (0, len(values)):
-                if (len(values) == 2):
-                    print 'Invalid format'
-                    quit()
+            if (int(values[i]) > 0):
+                pos.append(values[i])
+            else:
+                (neg.append(str(abs(int(values[i])))))
                 
-                if (int(values[i]) > 0):
-                    pos.append(values[i])
-                else:
-                    (neg.append(str(abs(int(values[i])))))
+        result += '\t\t\t"pos": ['
+        result += addListItems(pos)
+        
+        result += '\t\t\t"neg": ['
+        result += addListItems(neg)
+        
+        result = result[:-2] + '\n'
+        
+        result += '\t\t},\n'
                     
-            result += '\t\t\t"pos": ['
-            result += addListItems(pos)
-            
-            result += '\t\t\t"neg": ['
-            result += addListItems(neg)
-            
-            result = result[:-2] + '\n'
-            
-            result += '\t\t},\n'
-                
-                    
-result = result[:-2] + '\n\t],\n\t"version": "2"\n}'                
+result = result[:-2] + '\n\t],\n'
+
+if len(comments) != 0:
+    result += '\t"comments": [\n'
+    for ii, comment in enumerate(comments):
+        result += '\t\t"' + comment.replace('"', '\'') + '"'
+        if ii + 1 != len(comments):
+            result += ','
+        result += '\n'
+    result += '\t],\n'
+
+result += '\t"version": "2"\n'
+result += '}\n'
 
 text_file = open(sys.argv[2] + ".json", "w")
 text_file.write(result)
