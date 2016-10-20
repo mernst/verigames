67a97fd31495ac7e46bbf8e116e383137db23029
Seth Cooper <seth.cooper@gmail.com>
2016-10-20 11:47:12 -0400

    Add a few more DIMACS format checks.

--- flash/level_creation_scripts/normalize_dimacs.py
+++ flash/level_creation_scripts/normalize_dimacs.py
@@ -7,6 +7,7 @@ import sys
 
 cur_clause = []
 clause_count = 0
+max_var = 0
 header = None
 
 for line in sys.stdin.xreadlines():
@@ -16,16 +17,34 @@ for line in sys.stdin.xreadlines():
         continue
 
     if line[0] == '%':
+        if len(cur_clause) != 0:
+            raise RuntimeError('Comment during clause.')
         continue
 
     if line[0] == 'c':
+        if len(cur_clause) != 0:
+            raise RuntimeError('Comment during clause.')
+
         sys.stdout.write(line + '\n')
         continue
 
     if line[0] == 'p':
-        header = line
-        sys.stdout.write(line + '\n')
+        if header:
+            raise RuntimeError('Duplicate header: %s.' % (line))
+        if len(cur_clause) != 0:
+            raise RuntimeError('Header during clause.')
+        if clause_count != 0:
+            raise RuntimeError('Header after clause.')
+
+        header = line.split()
+        if len(header) != 4 or header[0] != 'p' or header[1] not in ['cnf', 'wcnf'] or not header[2].isdigit() or not header[3].isdigit():
+            raise RuntimeError('Bad header: %s.' % (line))
+
+        sys.stdout.write(' '.join(header) + '\n')
         continue
+
+    if not header:
+        raise RuntimeError('Clause before header.')
 
     for item in [int(x) for x in line.split()]:
         if item == 0:
@@ -34,17 +53,24 @@ for line in sys.stdin.xreadlines():
                 clause_count += 1
                 cur_clause = []
         else:
+            if header[1] == 'cnf' or (header[1] == 'wcnf' and len(cur_clause) > 0):
+                max_var = max(max_var, abs(item))
+
             cur_clause.append(item)
 
+
 if len(cur_clause) != 0:
-    sys.stderr.write('note: unterminated clause output\n')
-    sys.stdout.write(' '.join([str(x) for x in cur_clause]) + ' 0\n')
-    clause_count += 1
-    cur_clause = []
+    raise RuntimeError('Unterminated clause.')
 
 if header:
-    sys.stderr.write('header: ' + header + '\n')
+    sys.stderr.write('header: ' + ' '.join(header) + '\n')
 else:
-    sys.stderr.write('note: no header found\n')
+    raise RuntimeError('No header found.')
+
+if int(header[2]) != max_var:
+    raise RuntimeError('Variable count mismatch (expected %d, found %d).' % (int(header[2]), max_var))
+
+if int(header[3]) != clause_count:
+    raise RuntimeError('Clause count mismatch (expected %d, found %d).' % (int(header[3]), clause_count))
 
 sys.stderr.write('clauses output: ' + str(clause_count) + '\n')
