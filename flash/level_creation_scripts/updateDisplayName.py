98301e7c9f4f9018bf6cbf1eb70345b6540e1d17
Mackenzie Denker <mackenziedenker@gmail.com>
2017-04-11 12:49:11 -0400

    Added notes for creating word dictionary for level name hash.  Increased max node count.

--- flash/level_creation_scripts/updateDisplayName.py
+++ flash/level_creation_scripts/updateDisplayName.py
@@ -1,10 +1,17 @@
 import json, os, sys
 import _util
+from nltk.corpus import wordnet
 
 def update_display_name(level_json):
 	level_obj = json.load(open(level_json))
 	new_name = "jarvis" ## switch to using display name in future
 	##level_obj['file'] = new_name ##to be updated with correct name in future
+
+	## do this wordnet stuff in test file somewhere it can be run on it's own and iterated on quickly
+	## get wordnet or some sort of dictionary, use two adjectives followed by noun.
+
+	## take pieces of hash - find closest adjective to converted index for first two thirds, then same for noun for last third?
+	
 
 	_util.json_dump(level_obj, open(level_json, 'w'))
 
